889236273a1455b2a00540fdb023285e1419ce11
supatsara <supatsara.rodratsa@gmail.com>
2023-05-15 09:12:02 +0200

    expect test failing as the enpoints didn't implement yet

--- src/test/http.test.ts
+++ src/test/http.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest';
 import app from '../app';
 
 describe('Failing tests for API endpoints', () => {
-  it('POST /auth/signup should return a 404 Not Found', async () => {
+  it('POST /auth/signup should return a 200', async () => {
     const res = await request(app).post('/auth/signup').send({
       username: 'test',
       firstName: 'John',
@@ -10,59 +10,59 @@ describe('Failing tests for API endpoints', () => {
       email: 'john@email.com',
       password: '12345',
     });
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('POST /auth/login should return a 404 Not Found', async () => {
+  it('POST /auth/login should return a 200', async () => {
     const res = await request(app).post('/auth/login').send({
       username: 'test',
       password: '12345',
     });
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('POST /auth/logout should return a 404 Not Found', async () => {
+  it('POST /auth/logout should return a 200', async () => {
     const res = await request(app).post('/auth/logout');
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('GET /users should return a 404 Not Found', async () => {
+  it('GET /users should return a 200', async () => {
     const res = await request(app).get('/users');
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('GET /file/{userId} should return a 404 Not Found', async () => {
+  it('GET /file/{userId} should return a 200', async () => {
     const res = await request(app).get('/file/test');
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('POST /file/{userId} should return a 404 Not Found', async () => {
+  it('POST /file/{userId} should return a 200', async () => {
     const res = await request(app).post('/file/test').send();
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('GET /file/{userId}/{filename} should return a 404 Not Found', async () => {
+  it('GET /file/{userId}/{filename} should return a 200', async () => {
     const res = await request(app).post('/file/test/name').send();
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('DELETE /file/{userId}/{fileId} should return a 404 Not Found', async () => {
+  it('DELETE /file/{userId}/{fileId} should return a 200', async () => {
     const res = await request(app).post('/file/test/id').send();
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('POST /share/{userId}/{fileId} should return a 404 Not Found', async () => {
+  it('POST /share/{userId}/{fileId} should return a 200', async () => {
     const res = await request(app).post('/share/test/id').send();
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('PUT /share/{userId}/{fileId} should return a 404 Not Found', async () => {
+  it('PUT /share/{userId}/{fileId} should return a 200', async () => {
     const res = await request(app).put('/share/test/id').send();
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 
-  it('GET /share/{userId}/shared-files should return a 404 Not Found', async () => {
+  it('GET /share/{userId}/shared-files should return a 200', async () => {
     const res = await request(app).put('/share/test/shared-files').send();
-    expect(res.status).toBe(404);
+    expect(res.status).toBe(200);
   });
 });
