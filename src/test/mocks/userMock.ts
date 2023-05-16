64f6e356f7ffef906022ccad351d295aff533964
supatsara <supatsara.rodratsa@gmail.com>
2023-05-16 23:24:40 +0200

    create user mock for testing

---
+++ src/test/mocks/userMock.ts
@@ -0,0 +1,20 @@
+import { User } from '@/interfaces/user.type';
+
+export const UserMock: User[] = [
+  {
+    userId: '1',
+    username: 'test',
+    password: '12345',
+    firstName: 'test',
+    lastName: 'test',
+    email: 'test@gmail.com',
+  },
+  {
+    userId: '2',
+    username: 'test2',
+    password: '12345',
+    firstName: 'test2',
+    lastName: 'test2',
+    email: 'test_2@gmail.com',
+  },
+];
