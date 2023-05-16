35089229a89598044d24668123a4772aca077cd6
supatsara <supatsara.rodratsa@gmail.com>
2023-05-16 23:35:14 +0200

    Create user endpoint

---
+++ src/models/user.ts
@@ -0,0 +1,12 @@
+import { ObjectId } from 'mongodb';
+
+export default class User {
+  constructor(
+    public username: string,
+    public firstName: string,
+    public lastName: string,
+    public email: string,
+    public password: string,
+    public id?: ObjectId
+  ) {}
+}
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,12 +1,32 @@
+import {
+  HTTP_STATUS,
+  HTTP_STATUS_DESC,
+  HTTP_STATUS_ERROR_MSG,
+} from '@/constants/httpStatus.constant';
+import { verifyToken } from '@/middlewares/validateToken';
+import { collections } from '@/services/database.service';
 import express from 'express';
-// import { getLogger } from '@/utils/loggers';
 const router = express.Router();
-// const logger = getLogger('USER_ROUTE');
 
-/* GET users listing. */
-// router.get('/', function (_req, res) {
-//   logger.info('respond with a resource');
-//   res.send('respond with a resource');
-// });
+router.get('/', verifyToken, async function (_req, res) {
+  try {
+    // Get All Users
+    const documents = (await collections.users?.find().toArray()) || [];
+
+    return res.status(HTTP_STATUS.SUCCESS).json([...documents]);
+  } catch (err: unknown) {
+    if (err instanceof Error) {
+      console.error('Error:: ', err.message);
+    }
+
+    /**
+     * STATUS CODE: 500 INTERNAL SERVER ERROR
+     */
+    return res.status(HTTP_STATUS.INTERNAL_SERVER_ERROR).json({
+      description: HTTP_STATUS_DESC.INTERNAL_SERVER_ERROR,
+      error: HTTP_STATUS_ERROR_MSG.INTERNAL_SERVER_ERROR,
+    });
+  }
+});
 
 export default router;
