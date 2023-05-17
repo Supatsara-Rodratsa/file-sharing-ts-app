69b38963035fb5624d8517f7d34ab162d1ec2939
supatsara <supatsara.rodratsa@gmail.com>
2023-05-17 02:47:45 +0200

    Init File Collection

--- src/services/database.service.ts
+++ src/services/database.service.ts
@@ -1,14 +1,16 @@
 // External Dependencies
 import { MongoClient, ServerApiVersion, Db, Collection } from 'mongodb';
 // Global Variables
-export const collections: { users?: Collection } = {};
+export const collections: { users?: Collection; files?: Collection } = {};
+
 // Initialize Connection
 export async function connectToDatabase() {
   const url = process.env.DB_CONN_STRING;
   const users = process.env.USERS_COLLECTION_NAME;
+  const files = process.env.FILES_COLLECTION_NAME;
   const dbName = process.env.DB_NAME;
 
-  if (url && users && dbName) {
+  if (url && users && dbName && files) {
     const client: MongoClient = new MongoClient(url, {
       serverApi: {
         version: ServerApiVersion.v1,
@@ -21,11 +23,13 @@ export async function connectToDatabase() {
     const db: Db = client.db(dbName);
 
     const usersCollection: Collection = db.collection(users);
+    const filesCollection: Collection = db.collection(files);
 
     collections.users = usersCollection;
+    collections.files = filesCollection;
 
     console.log(
-      `Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}`
+      `Successfully connected to database: ${db.databaseName} and collection: ${usersCollection.collectionName}, ${filesCollection.collectionName}`
     );
   }
 }
