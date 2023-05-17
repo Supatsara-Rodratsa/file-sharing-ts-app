92c91aac7048adb1ccf73a60d5323744a53b938a
supatsara <supatsara.rodratsa@gmail.com>
2023-05-17 16:03:42 +0200

    Create File Collection mock

---
+++ src/test/mocks/fileMock.ts
@@ -0,0 +1,22 @@
+import File from '@/models/file';
+
+export const FileMock: File[] = [
+  {
+    id: '1',
+    filename: 'test',
+    size: 12345,
+    format: 'test',
+    url: 'test',
+    dateCreated: new Date().toString(),
+    ownerId: '1',
+  },
+  {
+    id: '2',
+    filename: 'test',
+    size: 12345,
+    format: 'test',
+    url: 'test',
+    dateCreated: new Date().toString(),
+    ownerId: '1',
+  },
+];
