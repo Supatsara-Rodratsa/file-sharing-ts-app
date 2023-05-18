74498098cc846fccf59dde9d59787838d8b1bb09
supatsara <supatsara.rodratsa@gmail.com>
2023-05-19 00:33:38 +0200

    Update aws secreat key

--- src/services/aws.service.ts
+++ src/services/aws.service.ts
@@ -9,7 +9,7 @@ class AWSService {
       region: process.env.AWS_REGION || '',
       credentials: {
         accessKeyId: process.env.AWS_ACCESS_KEY || '',
-        secretAccessKey: process.env.AWS_SECRET_KEY || '',
+        secretAccessKey: process.env.AWS_SECRET_KEY_1 || '',
       },
     });
   }
